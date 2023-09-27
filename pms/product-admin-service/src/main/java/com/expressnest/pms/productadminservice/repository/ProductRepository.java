48c26eb9ea3ab5c1230e052446319112f0247d44
neeharikasinghsjsu <143756330+neeharikasinghsjsu@users.noreply.github.com>
2023-09-27 14:38:59 -0700

    Delete pms directory

--- pms/product-admin-service/src/main/java/com/expressnest/pms/productadminservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expressnest.pms.productadminservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.expressnest.pms.productadminservice.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-    Product findByName(String name);
-}
--- pms/product-search-service/src/main/java/com/expressnest/pms/productsearchservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.expressnest.pms.productsearchservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.expressnest.pms.productsearchservice.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-	
-	Product findByName(String name);
-}
