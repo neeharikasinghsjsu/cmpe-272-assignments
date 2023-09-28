8a0d92e871770f44cd28d6607c7183a8e95010c7
Neeharika Singh <neeharikasingh@Neeharikas-Air.attlocal.net>
2023-09-28 10:44:50 -0700

    adding transaction

--- assignment-2/pms/product-admin-service/src/main/java/com/expressnest/pms/productadminservice/service/ProductService.java
+++ assignment-2/pms/product-admin-service/src/main/java/com/expressnest/pms/productadminservice/service/ProductService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 import com.expressnest.pms.productadminservice.entity.Product;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class ProductService {
     @Autowired
@@ -13,18 +15,21 @@ public class ProductService {
     @Autowired
     private ProductMessageService productMessageService;
 
+    @Transactional
     public Product saveProduct(Product product) {
     	Product savedProduct = productDbService.saveProduct(product);
     	productMessageService.publishCreateProduct(savedProduct);
         return savedProduct;
     }
 
+    @Transactional
     public String deleteProduct(int id) {
     	String deletedProduct = productDbService.deleteProduct(id);
     	productMessageService.publishDeleteProduct(id);
     	return deletedProduct;
     }
 
+    @Transactional
     public Product updateProduct(Product product) {
     	Product updatedProduct = productDbService.updateProduct(product);
     	productMessageService.publishUpdateProduct(updatedProduct);
